c0f7bb29a9009c98402c01014783551c409b420c
Batthula Rajeswari <n220848@rguktn.ac.in>
2026-06-25 02:35:51 +0000

    Refactor fake_keywords list formatting

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ news = st.text_area("Enter News Article")
 
 if st.button("Predict"):
 
-```
-fake_keywords = [
-    "shocking", "secret", "viral", "click here",
-    "conspiracy", "breaking", "unbelievable"
-]
+    fake_keywords = [
+        "shocking",
+        "secret",
+        "viral",
+        "click here",
+        "conspiracy",
+        "breaking",
+        "unbelievable"
+    ]
 
-if any(word in news.lower() for word in fake_keywords):
-    st.error("Prediction: FAKE NEWS")
-else:
-    st.success("Prediction: REAL NEWS")
-```
+    if any(word in news.lower() for word in fake_keywords):
+        st.error("Prediction: FAKE NEWS")
+    else:
+        st.success("Prediction: REAL NEWS")
